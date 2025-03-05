94d2039e7ded4c717855cc52b981a9a000042182
Christina Ju <christinajj314@gmail.com>
2025-03-05 11:08:54 -0800

    logout component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,38 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router";
-import "../styles/dashboard.css";
-import user from "../assets/user.svg";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import '../styles/dashboard.css';
+import user from '../assets/user.svg';
+import LogoutButton from './Logout';
 
 function NavBar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const navigate = useNavigate();
-  const userId = localStorage.getItem("userId");
+  const userId = localStorage.getItem('userId');
 
   // const handleLogOut = () => {
 
   // }
 
-
   return (
-    <nav className="navbar">
-      <div className="logo">NAME</div>
-     
-      <div className="user-menu">
+    <nav className='navbar'>
+      <div className='logo'>NAME</div>
+
+      <div className='user-menu'>
         <img
           src={user}
-          alt="User Icon"
-          className="user-icon"
+          alt='User Icon'
+          className='user-icon'
           onClick={() => setIsDropdownOpen(!isDropdownOpen)}
         />
 
         {isDropdownOpen && (
-          <div className="dropdown-content">
-            <button onClick={() => navigate(`/home/${userId}`)}>Dashboard</button>
-            <button onClick={() => navigate("/profile")}>Profile</button>
-            <button onClick={() => navigate("/")}>Log out</button>
+          <div className='dropdown-content'>
+            <button onClick={() => navigate(`/home/${userId}`)}>
+              Dashboard
+            </button>
+            <button onClick={() => navigate('/profile')}>Profile</button>
+            {/* <button onClick={() => navigate("/")}>Log out</button> */}
+            <LogoutButton />
           </div>
         )}
       </div>
