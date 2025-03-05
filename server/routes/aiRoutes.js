4426add776123b15662510631786d3c86e70bdff
Christina Ju <christinajj314@gmail.com>
2025-03-05 11:08:54 -0800

    logout component

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,9 +1,25 @@
 import express from 'express';
 import aiController from '../controllers/aiController.js';
-
+import { parseUserQuery } from './userQuery';
+import { queryOpenAIEmbedding, queryOpenAIChat } from './openaicontroller';
+import { queryPineconeDatabase } from './pinecone';
 const router = express.Router();
 
 // Route for submitting resume and query
 router.post('/submit-resume-query', aiController.handleResumeQuery);
+// POST endpoint for job recommendations
+router.post(
+  '/job-recommendations',
+  parseUserQuery,
+  queryOpenAIEmbedding,
+  queryPineconeDatabase,
+  queryOpenAIChat,
+  (req, res) => {
+    return res.status(200).json({
+      success: true,
+      recommendations: res.locals.jobRecommendations,
+    });
+  }
+);
 
-export default router; 
+export default router;
