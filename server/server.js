77bec9ad58c7e72908a4ee28d6c0dfa41587ba06
DonnaIsabel97 <tpr.th.bvb@gmail.com>
2025-03-05 11:04:38 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -1,31 +1,33 @@
-//server.js file is the entry point for the server, while app.js is the main file for the express app
-import app from './app.js';
-import connectDB from './database.js';
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./database.js";
 import cors from "cors";
-import userRoutes from './routes/userRoutes.js'
+import userRoutes from "./routes/userRoutes.js";
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 app.use(
-    cors({
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST", "DELETE", "PUT"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-      credentials: true,
-    })
-  );
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-app.use('/api', userRoutes)
+app.use("/api", userRoutes);
+
 // Start server independently of the database
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 // Connect to database independently of the server
 connectDB()
-    .then(() => console.log('Database connection established'))
-    .catch((error) => {
-        console.error('Database connection failed:', error);
-        // Optional: decide if you want to exit the process or continue running the server
-        // process.exit(1);
-    });
+  .then(() => console.log("Database connection established"))
+  .catch((error) => {
+    console.error("Database connection failed:", error);
+    // Optional: decide if you want to exit the process or continue running the server
+    // process.exit(1);
+  });
