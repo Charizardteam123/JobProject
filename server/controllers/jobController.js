31cca742fe845991e269e574effda73600834730
Christina Ju <christinajj314@gmail.com>
2025-03-04 16:10:08 -0800

    saveJob and deleteJob

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -60,8 +60,33 @@ const jobController = {
   },
 
   deleteJob: async (req, res) => {
+    const { userId, jobId } = req.body;
+    if (!userId || !jobId) {
+      return res.status(400).json({ error: 'User ID and Job ID are required' });
+    }
     try {
-    } catch (error) {}
+      // find user by userId
+      const user = await User.findById(userId);
+      // if user doesn't exist return error
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+      // if savedJob id doesn't exist on user object return error
+      if (!user.savedJobs.includes(jobId)) {
+        return res.status(400).json({ error: 'Job is not saved' });
+      }
+
+      user.savedJobs = user.savedJobs.filter(
+        (savedJob) => savedJob.toString() !== jobId
+      );
+      await user.save();
+      return res.status(200).json({
+        message: 'Job removed successfully',
+        savedJobs: user.savedJobs,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   },
 };
 
